b93f98f7993ba3a1bbb584567f3bc756ddad1201
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-03-26 22:03:35 +0530

    Generics

---
+++ src/Generics.ts
@@ -0,0 +1,9 @@
+function ElArray<T>(arr:T[]):T{
+     return arr[0];
+}
+
+
+ElArray(["harri"])
+ElArray([1,2,3,'hari'])
+
+//Enables you to create components that work with any data type while still providing compile-time type safety.
